f729d579f637f78faabadf6f24a7507569ca4d93
warzone-oier <wangrx_oier@foxmail.com>
2024-06-21 22:21:15 +0800

    ASDASD

--- letschat-server/Client.java
+++ letschat-server/Client.java
@@ -5,8 +5,9 @@ import java.security.KeyPair;
 public class Client extends Thread{
 	Network network;
 	Client(Socket s,KeyPair key) throws Exception{
-		network.keypair=key;
 		System.out.println("0");
+		network.keypair=key;
+		System.out.println("*");
 		if(network.connect(s)) throw new Exception();
 	}
 	public void run(){

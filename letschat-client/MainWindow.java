b94ffd5e22920fe6ee5c076e09a08947f42781dd
warzone-oier <wangrx_oier@foxmail.com>
2024-06-27 09:13:32 +0800

    12312312

--- letschat-client/MainWindow.java
+++ letschat-client/MainWindow.java
@@ -158,7 +158,7 @@ class OnlineUser extends ScrollPanel{
 public class MainWindow extends Thread{
 	static JFrame frame;
 	static Profile profile;
-	static OnlineUser OnlineUser;
+	static OnlineUser onlineUser;
 	static JSplitPane splitPane;
 	static String name;
 	MainWindow(){
@@ -170,10 +170,10 @@ public class MainWindow extends Thread{
 		frame.setVisible(false);
 		
 		profile=new Profile();
-		OnlineUser=new OnlineUser();
+		onlineUser=new OnlineUser();
 
 		splitPane=new JSplitPane(JSplitPane.VERTICAL_SPLIT,
-			profile.panel,OnlineUser.scrollPane);
+			profile.panel,onlineUser.scrollPane);
 		splitPane.setDividerLocation(75);
 		splitPane.setEnabled(false);
 		frame.add(splitPane);
@@ -187,7 +187,7 @@ public class MainWindow extends Thread{
 			profile.name.setText(s);
 			start();
 		}else{
-			OnlineUser.setUnvisible();
+			onlineUser.setUnvisible();
 			StartWindow.setVisble(true);
 		}
 	}
@@ -202,14 +202,14 @@ public class MainWindow extends Thread{
 				String name=ClientMain.server.receiveString();
 				System.out.println(name);
 				if(command[0]==Network.onlineUser)
-					SwingUtilities.invokeLater(()->OnlineUser.addUser(name));
+					SwingUtilities.invokeLater(()->onlineUser.addUser(name));
 				else if(command[0]==Network.offlineUser)
-					SwingUtilities.invokeLater(()->OnlineUser.deleteUser(name));
+					SwingUtilities.invokeLater(()->onlineUser.deleteUser(name));
 				else{
 					System.out.println("...");
 					String text=ClientMain.server.receiveString();
 					System.out.println("....");
-					SwingUtilities.invokeLater(()->OnlineUser.sendText(name,text));
+					SwingUtilities.invokeLater(()->onlineUser.sendText(name,text));
 				}
 				setPriority(5);
 				System.out.println("rrrrrr");
